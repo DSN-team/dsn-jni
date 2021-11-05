f6113961ddb801ea1d3380e509357b884e01f65e
assasinfil <nfa57643@gmail.com>
2021-11-05 23:35:19 +0300

    changed api

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.17
 
 require (
 	github.com/ClarkGuan/jni v0.0.0-20200615140459-56a06808b778
-	github.com/DSN-team/core v0.0.0-20211019122422-d2a8b9ebdc73
+	github.com/DSN-team/core v0.0.0-20211105194957-d2083a62a958
 )
 
 require github.com/mattn/go-sqlite3 v1.14.8 // indirect
--- jni.go
+++ jni.go
@@ -17,6 +17,7 @@ import (
 var workingVM jni.VM
 var callBackBufferPtr unsafe.Pointer
 var callBackBufferCap int
+var currentProfile core.Profile
 
 func main() {
 	fmt.Println("test")
@@ -28,15 +29,15 @@ func Java_com_dsnteam_dsn_CoreManager_initDB(env uintptr, _ uintptr) {
 }
 
 //export Java_com_dsnteam_dsn_CoreManager_register
-func Java_com_dsnteam_dsn_CoreManager_register(env uintptr, _ uintptr, usernameIn uintptr, passwordIn uintptr) (result bool) {
-	username, password := string(jni.Env(env).GetStringUTF(usernameIn)), string(jni.Env(env).GetStringUTF(passwordIn))
-	return core.Register(username, password)
+func Java_com_dsnteam_dsn_CoreManager_register(env uintptr, _ uintptr, usernameIn uintptr, passwordIn uintptr, addressIn uintptr) (result bool) {
+	username, password, address := string(jni.Env(env).GetStringUTF(usernameIn)), string(jni.Env(env).GetStringUTF(passwordIn)), string(jni.Env(env).GetStringUTF(addressIn))
+	return currentProfile.Register(username, password, address)
 }
 
 //export Java_com_dsnteam_dsn_CoreManager_login
 func Java_com_dsnteam_dsn_CoreManager_login(env uintptr, _ uintptr, pos int, passwordIn uintptr) (result bool) {
 	password := string(jni.Env(env).GetStringUTF(passwordIn))
-	return core.Login(password, pos)
+	return currentProfile.Login(password, pos)
 }
 
 //export Java_com_dsnteam_dsn_CoreManager_loadProfiles
@@ -65,35 +66,35 @@ func Java_com_dsnteam_dsn_CoreManager_getProfilesNames(env uintptr, _ uintptr) (
 
 //export Java_com_dsnteam_dsn_CoreManager_getProfilePublicKey
 func Java_com_dsnteam_dsn_CoreManager_getProfilePublicKey(env uintptr, _ uintptr) uintptr {
-	return jni.Env(env).NewString(core.GetProfilePublicKey())
+	return jni.Env(env).NewString(currentProfile.GetProfilePublicKey())
 }
 
 //export Java_com_dsnteam_dsn_CoreManager_getProfileName
 func Java_com_dsnteam_dsn_CoreManager_getProfileName(env uintptr, _ uintptr) uintptr {
-	return jni.Env(env).NewString(core.SelectedProfile.Username)
+	return jni.Env(env).NewString(currentProfile.Username)
 }
 
 //export Java_com_dsnteam_dsn_CoreManager_getProfileAddress
 func Java_com_dsnteam_dsn_CoreManager_getProfileAddress(env uintptr, _ uintptr) uintptr {
-	return jni.Env(env).NewString(core.SelectedProfile.Address)
+	return jni.Env(env).NewString(currentProfile.Address)
 }
 
 //export Java_com_dsnteam_dsn_CoreManager_addFriend
-func Java_com_dsnteam_dsn_CoreManager_addFriend(env uintptr, _ uintptr, addressIn uintptr, publicKeyIn uintptr) {
-	address, publicKey := string(jni.Env(env).GetStringUTF(addressIn)), string(jni.Env(env).GetStringUTF(publicKeyIn))
-	core.AddFriend(address, publicKey)
+func Java_com_dsnteam_dsn_CoreManager_addFriend(env uintptr, _ uintptr, usernameIn, addressIn, publicKeyIn uintptr) {
+	username, address, publicKey := string(jni.Env(env).GetStringUTF(usernameIn)), string(jni.Env(env).GetStringUTF(addressIn)), string(jni.Env(env).GetStringUTF(publicKeyIn))
+	currentProfile.AddFriend(username, address, publicKey)
 }
 
 //export Java_com_dsnteam_dsn_CoreManager_loadFriends
 func Java_com_dsnteam_dsn_CoreManager_loadFriends(env uintptr, _ uintptr) int {
-	return core.LoadFriends()
+	return currentProfile.LoadFriends()
 }
 
 //export Java_com_dsnteam_dsn_CoreManager_getFriendsIds
 func Java_com_dsnteam_dsn_CoreManager_getFriendsIds(env uintptr, _ uintptr) (ids uintptr) {
-	ids = jni.Env(env).NewIntArray(len(core.Friends))
-	for i := 0; i < len(core.Friends); i++ {
-		jni.Env(env).SetIntArrayElement(ids, i, core.Friends[i].Id)
+	ids = jni.Env(env).NewIntArray(len(currentProfile.Friends))
+	for i := 0; i < len(currentProfile.Friends); i++ {
+		jni.Env(env).SetIntArrayElement(ids, i, currentProfile.Friends[i].Id)
 	}
 	return
 }
@@ -102,9 +103,9 @@ func Java_com_dsnteam_dsn_CoreManager_getFriendsIds(env uintptr, _ uintptr) (ids
 func Java_com_dsnteam_dsn_CoreManager_getFriendsNames(env uintptr, _ uintptr) (usernames uintptr) {
 	//friends = getFriends()
 	dataType := jni.Env(env).FindClass("Ljava/lang/String;")
-	usernames = jni.Env(env).NewObjectArray(len(core.Friends), dataType, 0)
-	for i := 0; i < len(core.Friends); i++ {
-		jni.Env(env).SetObjectArrayElement(usernames, i, jni.Env(env).NewString(core.Friends[i].Username))
+	usernames = jni.Env(env).NewObjectArray(len(currentProfile.Friends), dataType, 0)
+	for i := 0; i < len(currentProfile.Friends); i++ {
+		jni.Env(env).SetObjectArrayElement(usernames, i, jni.Env(env).NewString(currentProfile.Friends[i].Username))
 	}
 	return
 }
@@ -113,9 +114,9 @@ func Java_com_dsnteam_dsn_CoreManager_getFriendsNames(env uintptr, _ uintptr) (u
 func Java_com_dsnteam_dsn_CoreManager_getFriendsAddresses(env uintptr, _ uintptr) (address uintptr) {
 	//friends = getFriends()
 	dataType := jni.Env(env).FindClass("Ljava/lang/String;")
-	address = jni.Env(env).NewObjectArray(len(core.Friends), dataType, 0)
-	for i := 0; i < len(core.Friends); i++ {
-		jni.Env(env).SetObjectArrayElement(address, i, jni.Env(env).NewString(core.Friends[i].Address))
+	address = jni.Env(env).NewObjectArray(len(currentProfile.Friends), dataType, 0)
+	for i := 0; i < len(currentProfile.Friends); i++ {
+		jni.Env(env).SetObjectArrayElement(address, i, jni.Env(env).NewString(currentProfile.Friends[i].Address))
 	}
 	return
 }
@@ -124,21 +125,21 @@ func Java_com_dsnteam_dsn_CoreManager_getFriendsAddresses(env uintptr, _ uintptr
 func Java_com_dsnteam_dsn_CoreManager_getFriendsPublicKeys(env uintptr, _ uintptr) (publicKey uintptr) {
 	//friends = getFriends()
 	dataType := jni.Env(env).FindClass("Ljava/lang/String;")
-	publicKey = jni.Env(env).NewObjectArray(len(core.Friends), dataType, 0)
-	for i := 0; i < len(core.Friends); i++ {
-		jni.Env(env).SetObjectArrayElement(publicKey, i, jni.Env(env).NewString(core.EncPublicKey(core.MarshalPublicKey(core.Friends[i].PublicKey))))
+	publicKey = jni.Env(env).NewObjectArray(len(currentProfile.Friends), dataType, 0)
+	for i := 0; i < len(currentProfile.Friends); i++ {
+		jni.Env(env).SetObjectArrayElement(publicKey, i, jni.Env(env).NewString(core.EncPublicKey(core.MarshalPublicKey(currentProfile.Friends[i].PublicKey))))
 	}
 	return
 }
 
 //export Java_com_dsnteam_dsn_CoreManager_connectToFriends
 func Java_com_dsnteam_dsn_CoreManager_connectToFriends(env uintptr, _ uintptr) {
-	core.ConnectToFriends()
+	currentProfile.ConnectToFriends()
 }
 
 //export Java_com_dsnteam_dsn_CoreManager_connectToFriend
 func Java_com_dsnteam_dsn_CoreManager_connectToFriend(env uintptr, _ uintptr, userId int) {
-	core.ConnectToFriend(userId)
+	currentProfile.ConnectToFriend(userId)
 }
 
 //Инициализировать структуры и подключение
@@ -151,7 +152,7 @@ func Java_com_dsnteam_dsn_CoreManager_runServer(env uintptr, _ uintptr, addressI
 		workingVM, _ = jni.Env(env).GetJavaVM()
 	}
 	core.UpdateUI = updateCall
-	core.RunServer(address)
+	currentProfile.RunServer(address)
 }
 
 //export Java_com_dsnteam_dsn_CoreManager_setCallBackBuffer
@@ -163,15 +164,19 @@ func Java_com_dsnteam_dsn_CoreManager_setCallBackBuffer(env uintptr, _ uintptr, 
 //export Java_com_dsnteam_dsn_CoreManager_writeBytes
 func Java_com_dsnteam_dsn_CoreManager_writeBytes(env uintptr, _ uintptr, inBuffer uintptr, lenIn int, userId int) {
 	log.Println("env write:", env)
-	defer runtime.KeepAlive(core.DataStrInput.Io)
+	defer runtime.KeepAlive(currentProfile.DataStrInput.Io)
 	point, size := jni.Env(env).GetDirectBufferAddress(inBuffer), jni.Env(env).GetDirectBufferCapacity(inBuffer)
 
-	sh := (*reflect.SliceHeader)(unsafe.Pointer(&core.DataStrInput.Io))
+	sh := (*reflect.SliceHeader)(unsafe.Pointer(&currentProfile.DataStrInput.Io))
 	sh.Data = uintptr(point)
 	sh.Len = lenIn
 	sh.Cap = size
 
-	core.WriteBytes(userId, lenIn)
+	currentProfile.WriteRequest(
+		userId,
+		core.BuildRequest(core.RequestData,
+			uint64(lenIn),
+			currentProfile.DataStrInput.Io))
 }
 
 //Realisation for platform
@@ -188,9 +193,9 @@ func updateCall(count int, userId int) {
 	sh := (*reflect.SliceHeader)(unsafe.Pointer(&bData))
 	sh.Data = uintptr(callBackBufferPtr)
 	sh.Cap = callBackBufferCap
-	sh.Len = len(core.DataStrOutput.Io)
+	sh.Len = callBackBufferCap
 	println("buffer pointer:", callBackBufferPtr)
-	copy(bData, core.DataStrOutput.Io)
+	copy(bData, currentProfile.DataStrOutput.Io)
 	println("buffer write done")
 	env.CallStaticVoidMethodA(classInput, methodId, jni.Jvalue(count), jni.Jvalue(userId))
 	workingVM.DetachCurrentThread()
